8f137966513aa82877ca7959a4c95ce0fa1eb5c1
Delfio <delfio_eu@hotmail.com>
2019-09-16 00:27:04 -0400

    Config Reactotron
    Reactotron configurado para melhor visualizar os dados da aplicação

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,9 @@
 import { createStore } from 'redux';
 
 import rootReducer from'./modules/rootReducer';
-const Store = createStore(rootReducer);
+
+const enhacer = process.env.NODE_ENV === 'development' ? console.tron.createEnhancer() : null;
+
+const Store = createStore(rootReducer, enhacer);
 
 export default Store;
